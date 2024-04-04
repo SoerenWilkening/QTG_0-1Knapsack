4ee8fed7de7dae9c786cfa2c5b0bba3d2e371cd1
Michael Perk <perk@ibr.cs.tu-bs.de>
2024-04-04 18:12:01 +0200

    Exclusive execution (due to out of memory errors)

--- evaluation/02_run_classical_benchmark.py
+++ evaluation/02_run_classical_benchmark.py
@@ -18,9 +18,9 @@ import slurminade
 slurminade.update_default_configuration(
     partition="alg",
     constraint="alggen03",
-    cpus_per_task=4,
-    mem_per_cpu="8G",
-    # exclusive=True,
+    #cpus_per_task=4,
+    #mem_per_cpu="8G",
+    exclusive=True,
     mail_type="FAIL",
 )  # global options for slurm
 
